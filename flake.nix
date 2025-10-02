e48ebd87c8d31cfc06c3b31b8d30299dbbf44833
Jeremie Alcaraz <hello@jeremiealcaraz.com>
2025-10-02 12:03:36 +0200

    Add QCOW image configuration and CI support

--- flake.nix
+++ flake.nix
@@ -32,52 +32,58 @@
     , ...
     } @ inputs:
     let
-      mkFormatter = system:
+      system = "x86_64-linux";
+      mkFormatter = system':
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = nixpkgs.legacyPackages.${system'};
         in
         pkgs.writeShellApplication {
           name = "fmt";
           runtimeInputs = [ pkgs.nixpkgs-fmt pkgs.findutils ];
           text = builtins.readFile ./scripts/fmt.sh;
         };
+      commonModules = [
+        ./host/nixos/configuration.nix
+
+        # Ajout de la compatibilité pour les binaires externes comme VS Code Server
+        ({ pkgs, ... }: {
+          programs.nix-ld = {
+            enable = true;
+            libraries = with pkgs; [
+              stdenv.cc.cc
+              zlib
+              curl
+              openssl
+            ];
+          };
+
+          # On garde le lingering, c'est une bonne pratique pour les services utilisateur
+          users.users.jeremie.linger = true;
+        })
+
+        # module Home-Manager
+        home-manager.nixosModules.home-manager
+
+        # glue pour charger la config user
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+
+          home-manager.users.jeremie = import ./home-manager/home.nix;
+        }
+      ];
     in
     rec {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./host/nixos/configuration.nix
+          inherit system;
+          modules = commonModules;
+        };
 
-            # Ajout de la compatibilité pour les binaires externes comme VS Code Server
-            ({ pkgs, ... }: {
-              programs.nix-ld = {
-                enable = true;
-                libraries = with pkgs; [
-                  stdenv.cc.cc
-                  zlib
-                  curl
-                  openssl
-                ];
-              };
-
-              # On garde le lingering, c'est une bonne pratique pour les services utilisateur
-              users.users.jeremie.linger = true;
-            })
-
-            # module Home-Manager
-            home-manager.nixosModules.home-manager
-
-            # glue pour charger la config user
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              # Ajoutez cette ligne pour passer inputs
-              home-manager.extraSpecialArgs = { inherit inputs; };
-
-              home-manager.users.jeremie = import ./home-manager/home.nix;
-            }
-          ];
+        nixos-qcow = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = commonModules ++ [ ./modules/system/image-qcow.nix ];
         };
       };
 
